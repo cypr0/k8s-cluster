7e0a1a7acd0471d2d3fad9a4d5ba4d431165436a
cypr0 <mail@philipp-rosch.de>
2024-06-09 14:00:05 +0200

    feat: add nextcloud

---
+++ kubernetes/apps/productivity/nextcloud/app/config/custom.config.php
@@ -0,0 +1,9 @@
+<?php
+          $CONFIG = array (
+            'default_language' => 'de',
+            'default_locale' => 'de_DE',
+            'default_phone_region' => 'DE',
+            'maintenance_window_start' => 1,
+            'allow_local_remote_servers' => true,
+            'bulkupload.enabled' => false,
+          );
